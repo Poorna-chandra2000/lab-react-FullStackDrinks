05f8ef93fc5154496cdceeb9de51770fd10abd31
Poorna Chandra2000 <poornachandra473@gmail.com>
2024-10-15 01:28:51 +0530

    Fullstack Drinks Internship done

---
+++ src/components/HomePage.js
@@ -0,0 +1,37 @@
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import beer3 from '../assets/bar-close-up-beer-52251475.webp';
+import beer2 from '../assets/beers.png';
+import beer from '../assets/nightlife-celebration-multi-colored-bar-establishment-generative-ai_188544-12375.avif';
+import './HomePage.css';
+
+const HomePage = () => {
+  return (
+    <div className="container">
+      <Link to="/beers">
+        <div id="all-beers" className="main-element">
+            <img src={beer2} alt="All Beers" />
+            <h2>All Beers</h2>
+            <p>Explore our vast collection of <br/> handcrafted beers from around the world.</p>
+        </div>
+      </Link>
+      <Link to="/random-beer">
+        <div id="random-beer" className="main-element">
+            <img src={beer} alt="Random Beer" />
+            <h2>Random Beer</h2>
+            <p>Discover a new favorite with <br/> our selection of random beers.</p>
+        </div>
+      </Link>
+      <Link to="/new-beer">
+        <div id="new-beer" className="main-element">
+            <img src={beer3} alt="New Beer" />
+            <h2>New Beer</h2>
+            <p>Create your own beer masterpiece with <br/> our easy-to-use new beer form.</p>
+        </div>
+      </Link>
+    </div>
+  );
+}
+
+export default HomePage;
